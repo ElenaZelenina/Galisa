d6c74eb670d0a665e69c480a388106bde65117d2
Elena Zelenina <zelenina.elena@gmail.com>
2019-07-09 12:14:53 -0400

    Add gallery with images

---
+++ scripts/gallery.js
@@ -0,0 +1,29 @@
+function initGallery() {
+    const gallery = {
+        images: document.querySelectorAll('div.gallery img'),
+        prevImage: document.querySelector('div.gallery i.prev'),
+        nextImage: document.querySelector('div.gallery i.next'),
+        navContainer: document.querySelector('div.gallery .navigation'),
+        currentIndex: 0,
+    };
+
+    gallery.galleryLength = gallery.images.length,
+
+        gallery.prevImage.onclick = function () {
+            gallery.currentIndex = gallery.currentIndex === 0 ? gallery.galleryLength - 1 : gallery.currentIndex - 1;
+            gallery.showCurrent();
+        }
+
+    gallery.nextImage.onclick = function () {
+        gallery.currentIndex = (gallery.currentIndex === (gallery.galleryLength - 1)) ? 0 : gallery.currentIndex + 1;
+        gallery.showCurrent();
+    }
+
+    gallery.showCurrent = function () {
+        for (let image of gallery.images) {
+            image.classList.remove('current');
+        }
+        gallery.images[gallery.currentIndex].classList.add('current');
+    }
+    return gallery;
+}
